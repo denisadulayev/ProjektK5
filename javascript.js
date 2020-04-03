a1578bc7a4c7c6ebea4f56d8f09897fd2efc3aa3
denisadulayev <61963450+denisadulayev@users.noreply.github.com>
2020-04-03 17:43:15 +0200

    Created Javascript for navbar and menubar

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function openWork() {
+    document.getElementById("workPage").style.height = "100%";
+  }
+  
+function closeWork() {
+    document.getElementById("workPage").style.height = "0%";
+}
