57a4f458f14ea195ea0ba62a692bfb6fad29990f
denisadulayev <61963450+denisadulayev@users.noreply.github.com>
2020-04-07 15:29:42 +0200

    Added functioning of clock

--- javascript.js
+++ javascript.js
@@ -15,3 +15,30 @@ function openWork() {
 function closeWork() {
     document.getElementById("work-page").style.height = "0%";
 }
+
+
+function showTime(){
+    const date = new Date();
+    let hour = date.getHours(); 
+    let minute = date.getMinutes(); 
+    let second = date.getSeconds(); 
+    
+    hour = (hour < 10) ? "0" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+    second = (second < 10) ? "0" + second : second;
+    
+    const time = hour + ":" + minute + ":" + second;
+    document.getElementById("timeDisplay").innerText = time;
+    document.getElementById("timeDisplay").textContent = time;
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+
+
+
+
+
+
+
